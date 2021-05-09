b46522e78d26480955fda54cf0a231f9c7244adf
Lazuardi Azhar <lazuardiazhar99@gmail.com>
2021-05-09 12:31:59 +0700

    Adding Idling Resource to test

--- app/src/main/java/com/dicoding/jetpack/academy/data/source/remote/RemoteDataSource.kt
+++ app/src/main/java/com/dicoding/jetpack/academy/data/source/remote/RemoteDataSource.kt
@@ -5,6 +5,7 @@ import android.os.Looper
 import com.dicoding.jetpack.academy.data.source.remote.response.ContentResponse
 import com.dicoding.jetpack.academy.data.source.remote.response.CourseResponse
 import com.dicoding.jetpack.academy.data.source.remote.response.ModuleResponse
+import com.dicoding.jetpack.academy.utils.EspressoIdlingResource
 import com.dicoding.jetpack.academy.utils.JsonHelper
 
 class RemoteDataSource private constructor(private val jsonHelper: JsonHelper){
@@ -24,15 +25,28 @@ class RemoteDataSource private constructor(private val jsonHelper: JsonHelper){
     }
 
     fun getAllCourses(callback: LoadCoursesCallback){
-        handler.postDelayed({ callback.onAllCoursesReceived(jsonHelper.loadCourses()) }, SERVICE_LATENCY_IN_MILLIS)
+        EspressoIdlingResource.increment()
+        handler.postDelayed({
+            callback.onAllCoursesReceived(jsonHelper.loadCourses())
+            EspressoIdlingResource.decrement()
+        }, SERVICE_LATENCY_IN_MILLIS)
     }
 
     fun getModules(courseId: String, callback: LoadModulesCallback) {
-        handler.postDelayed({ callback.onAllModulesReceived(jsonHelper.loadModule(courseId)) }, SERVICE_LATENCY_IN_MILLIS)
+        EspressoIdlingResource.increment()
+        handler.postDelayed({
+            callback.onAllModulesReceived(jsonHelper.loadModule(courseId))
+            EspressoIdlingResource.decrement()
+        }, SERVICE_LATENCY_IN_MILLIS)
+
     }
 
     fun getContent(moduleId: String, callback: LoadContentCallback) {
-        handler.postDelayed({ callback.onContentReceived(jsonHelper.loadContent(moduleId)) }, SERVICE_LATENCY_IN_MILLIS)
+        EspressoIdlingResource.increment()
+        handler.postDelayed({
+            callback.onContentReceived(jsonHelper.loadContent(moduleId))
+            EspressoIdlingResource.decrement()
+        }, SERVICE_LATENCY_IN_MILLIS)
     }
 
     interface LoadCoursesCallback {
